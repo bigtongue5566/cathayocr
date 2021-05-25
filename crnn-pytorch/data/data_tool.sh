6bfef16bf1ecf9edbccb2c68908fb340de4290f2
bigtongue5566 <bigtongue5566@gmail.com>
2021-05-25 21:44:00 +0800

    first commit

---
+++ crnn-pytorch/data/data_tool.sh
@@ -0,0 +1,2 @@
+trdg -l cn -c 1000 -w 5 -dt .\chars\ch_tra_char.txt --out_dir .\train\
+trdg -l cn -c 1000 -w 5 -dt .\chars\ch_tra_char.txt --out_dir .\dev\
---
+++ textdetect.sh
@@ -0,0 +1,2 @@
+python test.py --test_folder="images/" --output_folder="text_detect_result/"
+pip install -r requirements.txt
