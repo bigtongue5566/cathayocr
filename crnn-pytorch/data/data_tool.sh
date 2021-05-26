32ea70c3179b1ec50e29ea604a526c5753ee95ec
bigtongue5566 <bigtongue5566@gmail.com>
2021-05-26 19:49:21 +0800

    fix: change trdg args

--- crnn-pytorch/data/data_tool.sh
+++ crnn-pytorch/data/data_tool.sh
@@ -1,2 +1,2 @@
-trdg -l cn -c 1000 -w 5 -dt .\chars\ch_tra_char.txt --out_dir .\train\
-trdg -l cn -c 1000 -w 5 -dt .\chars\ch_tra_char.txt --out_dir .\dev\
+trdg -l cn -c 1000 -w 5 -bl 1 -rbl -dt ./chars/ch_tra_char.txt --out_dir ./train/
+trdg -l cn -c 1000 -w 5 -bl 1 -rbl -dt ./chars/ch_tra_char.txt --out_dir ./validate/
